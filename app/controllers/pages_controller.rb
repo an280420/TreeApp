0fd7a84d7d214f1596fd9948748539dd8d16f552
@an280420 <angit280420@gmail.com>
2021-12-04 21:03:35 +0500

    edit views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,46 @@ class PagesController < ApplicationController
   end
 
   def edit
+    @page = Page.find params[:id]
   end
 
   def new
     @page = Page.new
+  end
+
+  def create
+    # render plain: params
+    @page = Page.new page_params
+    if @page.save
+      flash[:success] = 'Страница успешно создана!'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @page = Page.find params[:id]
+    if @page.update page_params_update
+      flash[:success] = 'Страница успешно обновлена!'
+      redirect_to page_path
+    else
+      render :edit
+    end
+  end
+
+
+  def show
+    @page = Page.find params[:id]
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:name, :title, :body)
+  end
+
+  def page_params_update
+    params.require(:page).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  
+  resources :pages
 
   get 'pages/add', to: 'pages#new'
-  get 'pages/edit', to: 'pages#edit'
+  get 'pages/:id/edit', to: 'pages#edit'
+  post 'pages/:id', to: 'pages#update'
   get '/add', to: 'pages#new'
+  post 'pages', to: 'pages#create'
+  get 'pages/:id', to: 'pages#show'
+
 end
