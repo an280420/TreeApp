4fd385bc5a913d9897a76f9ba9ec13f049f836da
@an280420 <angit280420@gmail.com>
2021-12-04 15:29:34 +0500

    add new edit actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
   end
+
+  def edit
+  end
+
+  def new
+    @page = Page.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  get 'pages/add', to: 'pages#new'
+  get 'pages/edit', to: 'pages#edit'
 end
